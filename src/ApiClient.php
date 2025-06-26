d3dc7f54194648f72f98a18851a477678e440624
sion <sionix1@gmail.com>
2025-06-26 17:13:30 +0200

    - Add `generateApplicantReport` method in `ApiClient` - Define `PATH_GENERATE_REPORT` constant for generating applicant reports - Provide functionality to retrieve and return report data for a given applicant ID

--- src/ApiClient.php
+++ src/ApiClient.php
@@ -44,6 +44,7 @@ class ApiClient
     private const string PATH_SANDBOX_CHECK_COMPLETE = 'resources/applicants/%s/status/testCompleted';
     private const string PATH_CLARIFY_REJECTION_REASON = 'resources/moderationStates/-;applicantId=%s';
     private const string PATH_GENERATE_EXTERNAL_WEBSDK_LINK = 'resources/sdkIntegrations/levels/-/websdkLink';
+    private const string PATH_GENERATE_REPORT = 'resources/applicants/%s/summary/report?lang=en&report=applicantReport&waitForId=report-ready';
 
     protected ?Client $apiClient = null;
 
@@ -106,6 +107,17 @@ class ApiClient
         $response = $this->getApiClient()->post(static::PATH_GENERATE_EXTERNAL_WEBSDK_LINK, $request->toGuzzleOptions());
 
         return ExternalWebSDKLink::fromResponse($response);
+    }
+
+    /**
+     * @throws GuzzleException
+     * @throws \DateMalformedStringException
+     */
+    public function generateApplicantReport(string $applicationId): string
+    {
+        $response = $this->getApiClient()->get(sprintf(static::PATH_GENERATE_REPORT, $applicationId));
+
+        return (string) $response->getBody();
     }
 
     /**
