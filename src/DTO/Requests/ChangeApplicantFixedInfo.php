141bcb6be2f49a096a51b6df9a9e9a732ca619bb
sion <sionix1@gmail.com>
2025-07-02 13:35:16 +0200

    - Refactor `ChangeApplicantFixedInfo` to use `ApplicantFixedInfo` as a dependency - Simplify constructor by removing redundant fields - Update `toGuzzleOptions` to filter `ApplicantFixedInfo` properties directly

--- src/DTO/Requests/ChangeApplicantFixedInfo.php
+++ src/DTO/Requests/ChangeApplicantFixedInfo.php
@@ -7,47 +7,19 @@ namespace SumsubApi\DTO\Requests;
 use GuzzleHttp\RequestOptions;
 use SumsubApi\DTO\BaseRequest;
 use SumsubApi\DTO\Parts\ApplicantFixedInfo;
-use SumsubApi\Enums\Gender;
 
-readonly class ChangeApplicantFixedInfo extends ApplicantFixedInfo implements BaseRequest
+readonly class ChangeApplicantFixedInfo implements BaseRequest
 {
     public function __construct(
         public string $applicantId,
-        ?string $firstName = null,
-        ?string $middleName = null,
-        ?string $lastName = null,
-        ?string $legalName = null,
-        ?Gender $gender = null,
-        ?\DateTimeInterface $dob = null,
-        ?string $placeOfBirth = null,
-        ?string $countryOfBirth = null,
-        ?string $stateOfBirth = null,
-        ?string $country = null,
-        ?string $nationality = null,
-        ?array $addresses = null,
-        ?string $tin = null,
+        public ApplicantFixedInfo $applicantFixedInfo,
     ) {
-        parent::__construct(
-            firstName: $firstName,
-            middleName: $middleName,
-            lastName: $lastName,
-            legalName: $legalName,
-            gender: $gender,
-            dob: $dob,
-            placeOfBirth: $placeOfBirth,
-            countryOfBirth: $countryOfBirth,
-            stateOfBirth: $stateOfBirth,
-            country: $country,
-            nationality: $nationality,
-            addresses: $addresses,
-            tin: $tin,
-        );
     }
 
     public function toGuzzleOptions(): array
     {
         return [
-            RequestOptions::JSON => array_filter($this->toArray()),
+            RequestOptions::JSON => array_filter($this->applicantFixedInfo->toArray(), fn (mixed $value) => !is_null($value)),
         ];
     }
 }
